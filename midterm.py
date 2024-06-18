0e1afc24aed9d29bbb243217d66ebc05a30293b2
Kyle Bradshaw <itllbok82@gmail.com>
2024-06-18 10:38:26 -0400

    working on midterm

--- midterm.py
+++ midterm.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-ncvoter7 = 'C:/Users/Owner/PycharmProjects/Midterm_Code/Data/ncvoter7.txt'
+ncvoter7 = '/Users/kylebradshaw/repos/midterm/Data/ncvoter7.txt'
 
-beaufort_voter = 'C:/Users/Owner/PycharmProjects/Midterm_Code/Data/beaufort_voter.csv'
+beaufort_voter = '/Users/kylebradshaw/repos/midterm/Data/beaufort_voter.csv'
 
 df = pd.read_csv(ncvoter7, delimiter='\t')
 
@@ -10,4 +10,5 @@ df = pd.read_csv(beaufort_voter, delimiter=',')
 
 active_voters_df = df[df['voter_status_desc'] == 'ACTIVE']
 
-filtered_pile_path = 'C:/Users/Owner/PycharmProjects/Midterm_Code/filtered_active_voters.csv'
+filtered_file_path = '/Users/kylebradshaw/repos/midterm/filtered_active_voters.csv'
+active_voters_df.to_csv(filtered_file_path, index=False)
