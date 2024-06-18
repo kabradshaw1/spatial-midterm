3e0fe71a557b38e36f4d2d7c7850dd6207d5dba8
Kyle Bradshaw <itllbok82@gmail.com>
2024-06-18 14:18:18 -0400

    working on midterm

--- print.py
+++
@@ -1,9 +0,0 @@
-import geopandas as gpd
-
-shapefile_path = 'C:/Users/kylebradshaw/repos/midterm/Data/Beaufort_Count_Streets/addresses.shp'
-
-# Read the shapefile into a GeoDataFrame
-gdf = gpd.read_file(shapefile_path)
-
-# Print the contents of the GeoDataFrame
-print(gdf)
